ee5a53e4195cdb44b1330528efc32680716e057c
phorjax <joseph.12082@gmail.com>
2023-02-08 15:48:39 +0000

    New

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+};
+let excuseGen = () => {
+  let who = [
+    "my cousin",
+    "my mom",
+    "the dog",
+    "the cat",
+    "my fish",
+    "my grandma"
+  ];
+  var whoExcuse = who[Math.floor(Math.random() * 5)];
+  let action = ["ate", "cut", "demolished", "shrink", "managed", "guided"];
+  var actionExcuse = action[Math.floor(Math.random() * 5)];
+  let what = [
+    " the homework",
+
+    "the car",
+    "the computer",
+    "my shoes",
+
+    "the keys",
+    "the code"
+  ];
+  var whatExcuse = what[Math.floor(Math.random() * 5)];
+  let when = [
+    "before the gym",
+    "before class",
+    "after class",
+    "on class",
+    "right on time",
+    "on the weekend"
+  ];
+  var whenExcuse = when[Math.floor(Math.random() * 5)];
+
+  return whoExcuse + actionExcuse + whatExcuse + whenExcuse;
+  console.log(excuseGen);
 };
